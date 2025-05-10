06be3161fa000f214989bda988389c59cf57a8af
agyesam12 <agyesam12@gmail.com>
2025-05-10 01:46:09 +0000

    added other files

--- loans_application/settings.py
+++ loans_application/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0jzrc*w1+*p7vk)9fc7gi%no_7o1jwsi7f-oz$2$p1i$v7p6s$'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
